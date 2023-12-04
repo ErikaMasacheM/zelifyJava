223665e44a06d7e44c1c78c64a9764ae9a005c2d
ErikaMasacheM <92096749+ErikaMasacheM@users.noreply.github.com>
2023-12-04 14:23:34 -0500

    levantar kafka

---
+++ src/main/java/com/zwippetech/kafka/config/KafkaTopicConfig.java
@@ -0,0 +1,14 @@
+package com.zwippetech.kafka.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+    public NewTopic codeTopic(){
+        return TopicBuilder.name("ejercicio")
+        .build();
+    }
+}
+
